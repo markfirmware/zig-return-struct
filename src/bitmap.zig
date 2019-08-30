16189efae125a167b39f914de7bdd32a594723f5
markfirmware <markfirmware@users.noreply.github.com>
2019-08-30 10:35:37 -0400

    .

--- src/bitmap.zig
+++ src/bitmap.zig
@@ -2,9 +2,11 @@ pub const Bitmap = struct {
     frame_buffer: *u8,
     pixel_array: [*]u8,
     width: u32,
+    another: u32,
 
     pub fn init() Bitmap {
         var bm: Bitmap = undefined;
+        bm.width = 100;
         return bm;
     }
 };
