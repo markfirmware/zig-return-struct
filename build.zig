8d57e3eccd411f4c3d05ef94181579f183378409
markfirmware <markfirmware@users.noreply.github.com>
2019-08-30 10:08:14 -0400

    .

--- build.zig
+++ build.zig
@@ -1,8 +1,23 @@
 const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
+const std = @import("std");
 
 pub fn build(b: *Builder) void {
+    const want_arm64 = b.option(bool, "arm64", "Build for arm64") orelse false;
+
+    var exe: *std.build.LibExeObjStep = undefined;
+    if (want_arm64) {
+        exe = b.addExecutable("zig-return-struct-arm64", "src/main.zig");
+        exe.setTarget(
+            builtin.Arch{ .aarch64 = builtin.Arch.Arm64.v8 },
+            builtin.Os.linux,
+            builtin.Abi.gnu,
+        );
+    } else {
+        exe = b.addExecutable("zig-return-struct-x86_64", "src/main.zig");
+    }
+
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("zig-return-struct", "src/main.zig");
     exe.setBuildMode(mode);
     exe.setOutputDir(".");
     exe.install();
