baf13e13047f88ea92a53b0d7c795fc3281b96ad
markfirmware <markfirmware@users.noreply.github.com>
2019-08-30 10:32:09 -0400

    .

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,15 @@
 pub fn main() anyerror!void {
     a = Bitmap.init();
     std.debug.warn("{}\n", a);
+    b = Bitmap.init();
+    std.debug.warn("{}\n", b);
+    c = Bitmap.init();
+    std.debug.warn("{}\n", c);
 }
 
 var a: Bitmap = undefined;
+var b: Bitmap = undefined;
+var c: Bitmap = undefined;
 
 const Bitmap = @import("bitmap.zig").Bitmap;
 const std = @import("std");
