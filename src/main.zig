cd1448e4737a8d8b5bbf357aa3ecec40980abcb0
markfirmware <markfirmware@users.noreply.github.com>
2019-10-24 12:44:55 -0400

    .

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,15 @@
-pub fn main() anyerror!void {
-//  var current_el = asm("mrs %[current_el], CurrentEL"
-//      : [current_el] "=r" (-> usize));
-//  warn("CurrentEL {x} exception level {}\n", current_el, current_el >> 2 & 0x3);
-
-    var x: u32 = 1;
-    var y: u32 = 0;
-    var z = x / y;
-
-    a = Bitmap.init();
-    std.debug.warn("{}\n", a);
-    b = Bitmap.init();
-    std.debug.warn("{}\n", b);
-    c = Bitmap.init();
-    std.debug.warn("{}\n", c);
+pub fn main() void {
+    pollData();
 }
 
-var a: Bitmap = undefined;
-var b: Bitmap = undefined;
-var c: Bitmap = undefined;
+fn pollData() void {
+    if (poll()) |event_data| {
+        var buf = event_data;
+        const rssi = buf[buf.len - 1]; // this is required to break
+    }
+}
 
-const Bitmap = @import("bitmap.zig").Bitmap;
-const std = @import("std");
-const warn = @import("std").debug.warn;
-
-const release_tag = "0.1";
+pub var poll_data_buf: [50]u8 = undefined;
+pub fn poll() ?[]u8 {
+    return poll_data_buf[0..];
+}
