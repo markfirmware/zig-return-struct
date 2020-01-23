700d913120a5b4a7020723699a1f09d9d781e2cb
markfirmware <markfirmware@users.noreply.github.com>
2020-01-22 19:15:17 -0500

    .

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,13 @@
 
 pub fn main() void {
     warn("program start\n", .{});
+    var sctlr = asm("mrs %[sctlr], sctlr_el0"
+        : [sctlr] "=r" (-> usize));
+    warn("sctlr {x}\n", sctlr);
     asm volatile("ldur q0, [sp, #-24]");
 //  var d0_register = asm("vmov %[d0_register], d0"
 //      : [d0_register] "=r" (-> usize));
-    warn("program end - no exceptions encountere\n", .{});
+    warn("program end - no exceptions encountered\n", .{});
 }
 
 const warn = @import("std").debug.warn;
