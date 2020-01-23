5ce368ea8d18f64551e85188cb078f9e5500d0cc
markfirmware <markfirmware@users.noreply.github.com>
2020-01-22 19:17:41 -0500

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ pub fn main() void {
     warn("program start\n", .{});
     var sctlr = asm("mrs %[sctlr], sctlr_el0"
         : [sctlr] "=r" (-> usize));
-    warn("sctlr {x}\n", sctlr);
+    warn("sctlr {x}\n", .{sctlr});
     asm volatile("ldur q0, [sp, #-24]");
 //  var d0_register = asm("vmov %[d0_register], d0"
 //      : [d0_register] "=r" (-> usize));
